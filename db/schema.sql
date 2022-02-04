54ba9d92b4d5010eda50cb849cd2a9f7da4060ce
Sajees XPS <sajees.jeya@gmail.com>
2022-02-04 01:58:44 -0500

    updated parties

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,12 @@
+DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS parties;
+
+CREATE TABLE parties (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    description TEXT
+); 
+
 CREATE TABLE candidates (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
